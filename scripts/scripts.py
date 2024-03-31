62abd80e379c0d193bfedb2a8cc9420d89f171e4
david-istvan <goerenh@gmail.com>
2024-03-31 15:30:07 +0200

    Map colorbar size fixed

--- scripts/scripts.py
+++ scripts/scripts.py
@@ -55,32 +55,32 @@ def worldMap():
     papersPerCountry = countries.groupby('country').count().sort_values(by=countries.columns[0], ascending=False).reset_index()
     papersPerCountry.columns = ['country', 'papers']
     
-    print(papersPerCountry)
+    #print(papersPerCountry)
     
     #papersPerCountry[(papersPerCountry['country'] == 'USA')]['country'] = 'United States of America'
     
     papersPerCountry['country'] = papersPerCountry['country'].str.replace('USA','United States of America')
     papersPerCountry['country'] = papersPerCountry['country'].str.replace('UK','United Kingdom')
     
-    print(papersPerCountry)
+    #print(papersPerCountry)
 
     world = gpd.read_file(gpd.datasets.get_path('naturalearth_lowres'))
     ax2 = world.plot(figsize=(15,15), edgecolor=u'white', color='gray')
     
     #countries = ['Germany', 'Norway', 'Russia', 'China', 'Japan']
     
-    print('Australia' in papersPerCountry['country'].values)
+    #print('Australia' in papersPerCountry['country'].values)
     
-    print('-----')
-    print(int(papersPerCountry[papersPerCountry['country'] == 'Australia']['papers']))
-    print('-----')
+    #print('-----')
+    #print(int(papersPerCountry[papersPerCountry['country'] == 'Australia']['papers']))
+    #print('-----')
     
     world['papers'] = world.apply(lambda row: int(papersPerCountry[(papersPerCountry['country'] == row['name'])]['papers']) if (row['name'] in papersPerCountry['country'].values) else np.nan, axis=1)
     
     
-    print(world['papers'])
+    #print(world['papers'])
     
-    world.plot(column = 'papers', edgecolor=u'white', cmap='plasma', ax=ax2, legend = True, missing_kwds={'color': '#ededed'})
+    world.plot(column = 'papers', edgecolor=u'white', cmap='plasma', ax=ax2, legend = True, missing_kwds={'color': '#ededed'}, legend_kwds={'shrink': 0.66})
     #.loc[world['name'].isin(papersPerCountry['country'])]
     
     ax2.axis('scaled')
